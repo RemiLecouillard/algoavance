7bfa8fe576d5431daadb2902cfaffdbb08e088c7
RemiLecouillard <remizbay@hotmail.fr>
2018-01-29 08:55:52 +0100

    commit

--- tp2/abr.c
+++ tp2/abr.c
@@ -5,24 +5,29 @@
 #include <stdlib.h>
 
 
-/*****************************************************************************/
-NOEUD *arbre_vide() {return NULL; }
-/*****************************************************************************/
-NOEUD *insere(NOEUD *p, element x)
-{if (p == NULL)
-    {p = (NOEUD *)malloc(sizeof(NOEUD));
+NOEUD *arbre_vide() {
+  return NULL;
+}
+
+NOEUD *insere(NOEUD *p, element x) {
+  if (p == NULL){
+    p = (NOEUD *)malloc(sizeof(NOEUD));
      p->valeur = x;p->gauche = NULL;p->droit = NULL;
     }
- else if (x == p->valeur) printf("%d est deja dans l'arbre\n",x);
-      else if (x < p->valeur)  p->gauche = insere(p->gauche,x);
-                          else p->droit  = insere(p->droit,x);
+ else if (x == p->valeur){
+   printf("%d est deja dans l'arbre\n",x);
+ } else if (x < p->valeur){
+    p->gauche = insere(p->gauche,x);
+  } else {
+     p->droit  = insere(p->droit,x);
+  }
 
-return(p);
+  return(p);
 }
-/*****************************************************************************/
-void affiche_arbre(NOEUD *p, int col)
-{int i;
- if (p)
+
+void affiche_arbre(NOEUD *p, int col) {
+  int i;
+  if (p)
     {affiche_arbre(p->droit,col+1);
      for (i=0;i<col;i++) printf("   ");
      printf("%d\n",p->valeur);
--- tp2/abr.h
+++ tp2/abr.h
@@ -10,14 +10,13 @@ typedef int element;
 typedef struct noeud {
 	element valeur;
 	struct noeud *gauche, *droit;
-	} NOEUD, *ABR;
+} NOEUD, *ABR;
 
 
-/*****************************************************************************/
 NOEUD *arbre_vide();
-/*****************************************************************************/
+
 NOEUD *insere(NOEUD *p, element x);
-/*****************************************************************************/
+
 void affiche_arbre(NOEUD *p, int col);
 
 #endif
